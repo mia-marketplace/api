0e3c86307e45984d82ca554d51c34d828c27c2c2
uzoma <chidumaga@gmail.com>
2019-01-29 08:27:14 +0100

    modifies jwt blacklists migration file

--- db/migrate/20190127173518_create_jwt_blacklists.rb
+++ db/migrate/20190127173518_create_jwt_blacklists.rb
@@ -2,7 +2,7 @@ class CreateJwtBlacklists < ActiveRecord::Migration[5.2]
   def change
     create_table :jwt_blacklist do |t|
     	t.string :jti, null: false
-    	t.datetime :expired, null: false
+    	t.datetime :exp, null: false
     end
     add_index :jwt_blacklist, :jti
   end
